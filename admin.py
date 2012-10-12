f04ec9cf9f173f7a07b78f2f99d6406fc00d0f40
MontesDaLua <miguel.b.rodrigues@gmail.com>
2012-10-12 10:18:00 +0100

    version 0.2 20121010162327

--- admin.py
+++ admin.py
@@ -1,5 +1,5 @@
 #sku/admin.py 
-#Automaticly Generated at 20121009114548
+#Automaticly Generated at 20121010094227
 from django.contrib import admin 
 from sku.models import Sku
 admin.site.register(Sku) 
