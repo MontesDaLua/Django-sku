734d8fde7fce21ab91e68c3fea41bba292977c9c
MontesDaLua <miguel.b.rodrigues@gmail.com>
2012-10-17 08:49:49 +0100

    version 0.2 20121017081214

--- tests.py
+++ tests.py
@@ -1,16 +1,39 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+unittest for Sku module 
 """
 
+from django.utils import unittest
+from models import Sku
 from django.test import TestCase
 
+class SkuTestCase(unittest.TestCase):
+    def setUp(self):
+        self.sku1 = Sku.objects.create( IntCode = "Codigo 1", MovUnit = '', Desc = "Descricao Codigo 1", ShortDesc = "Descricao Curta Codigo 1") 
+    	self.sku1.LoadSampleData(10)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+#
+# Database Tests 
+#
+
+    def testDbSku(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Test sky object single creation 
         """
-        self.assertEqual(1 + 1, 2)
+        self.assertEqual(self.sku1.IntCode, 'Codigo 1')
+
+    def testDbLoadSku(self):
+        """
+        Test sky object single creation 
+        """
+
+    def testDbt3(self):
+        """
+        Test sky object single creation 
+        """
+#
+# Web site tests
+#
+    def testWst3(self):
+        """
+        Test sky object single creation 
+        """
