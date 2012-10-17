85a76f3273cea6fecdab5807ec4e7d7b1933b0a3
MontesDaLua <miguel.b.rodrigues@gmail.com>
2012-10-17 11:53:30 +0100

    version 0.2 20121017110425

--- admin.py
+++ admin.py
@@ -1,5 +1,5 @@
 #sku/admin.py 
-#Automaticly Generated at 20121016164828
+#Automaticly Generated at 20121017085024
 from django.contrib import admin 
 from sku.models import Sku
 admin.site.register(Sku) 
--- tests.py
+++ tests.py
@@ -3,13 +3,17 @@ unittest for Sku module
 """
 
 from django.utils import unittest
-from models import Sku
 from django.test import TestCase
+from django.test.client import RequestFactory, Client 
+from django.contrib.auth.models import User
+from models import Sku
 
 class SkuTestCase(unittest.TestCase):
     def setUp(self):
         self.sku1 = Sku.objects.create( IntCode = "Codigo 1", MovUnit = '', Desc = "Descricao Codigo 1", ShortDesc = "Descricao Curta Codigo 1") 
-    	self.sku1.LoadSampleData(10)
+        self.sku1.LoadSampleData(10)
+        self.c = Client()
+
 
 #
 # Database Tests 
@@ -33,7 +37,30 @@ class SkuTestCase(unittest.TestCase):
 #
 # Web site tests
 #
-    def testWst3(self):
+    def testWst1(self):
         """
-        Test sky object single creation 
+        Authentication
         """
+        self.assertEqual( self.c.login(username='fred', password='secret'), False)
+        self.uname = 'okuser'
+        self.PASS = 'lixo'
+	User.objects.create_user(self.uname, "ll@datreta.com", self.PASS)
+        self.assertEqual( self.c.login(username=self.uname, password=self.PASS), True )
+
+    def testWst2(self):
+        """
+        Defined Views
+        """
+	#
+	# generic 
+	r = self.c.get('/sku/')
+        self.assertEqual( r.status_code, 404  )
+	#
+	# Existing sku
+	r = self.c.get('/sku/ver/'+ str(self.sku1.id) +'/')
+        self.assertEqual( r.status_code, 200  )
+	#
+	# Non Existing sku
+	r = self.c.get('/sku/ver/5555/')
+        self.assertEqual( r.status_code, 404  )
+	
